780c3639535f23eadbdcb12acd65598a16104b52
dustinvrdies <dustin82895@gmail.com>
2026-07-25 20:34:23 -0400

    Pass 1: add web landing page foundation

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>DAH Attention Exchange</h1>
+      <p>Building the future of transparent attention rewards.</p>
+    </main>
+  );
+}
