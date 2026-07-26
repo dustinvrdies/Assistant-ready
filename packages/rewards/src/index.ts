f34d4d360cea9c9f5a4dea241ab853429b240f00
dustinvrdies <dustin82895@gmail.com>
2026-07-25 20:39:22 -0400

    Pass 1: expand reward package exports

--- packages/rewards/src/index.ts
+++ packages/rewards/src/index.ts
@@ -1,7 +1,21 @@
+export * from "./events";
+
 export interface AttentionReward {
   userId: string;
   secondsWatched: number;
   reward: number;
+}
+
+export interface RewardEngineConfig {
+  baseRate: number;
+}
+
+export function createRewardEngine(config: RewardEngineConfig) {
+  return {
+    calculate(seconds: number) {
+      return Number((seconds * config.baseRate).toFixed(6));
+    },
+  };
 }
 
 export function calculateReward(secondsWatched: number): number {
